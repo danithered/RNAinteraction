33521497538ad2f200e395cda06c34a10e182f5e
danithered <danithered@live.com>
2024-11-26 15:41:47 +0100

    started to put it into fn
    	modified:   list.c

--- list.c
+++ list.c
@@ -5,10 +5,7 @@
 #include <ViennaRNA/subopt/wuchty.h>  
 #include <ViennaRNA/utils/basic.h>
 
-int main(int argc, char** argv){
-	char rna1[] = "AAAAAAAACCCCC\0";
-	char rna2[] = "UUUUGGG\0";
-
+vrna_subopt_solution_t* fn3(char *rna1, char *rna2, const double temperature){
 	// dynamic allocation for concatenated string
 	const unsigned int length1=strlen(rna1), length2=strlen(rna2);
 	const unsigned int length=length1+length2+1;
@@ -17,7 +14,7 @@ int main(int argc, char** argv){
 	char *cstr = (char*) calloc(length+1, sizeof(char)); // while the doc says that it does not need it, vrna_dimer still uses this memory
 	if(!(concatenated && cstr)){
 		printf("could not allocate memory is size sizeof(char) * %d\n", length+1);
-		return(2);
+		return(NULL);
 	}
 	strcpy(concatenated, rna1);
 	concatenated[length1] = '&';
@@ -29,7 +26,7 @@ int main(int argc, char** argv){
 	char* constraint = (char*) calloc(length+1, sizeof(char));
 	if(!constraint){
 		printf("could not allocate memory is size sizeof(char) * %d\n", length+1);
-		return(2);
+		return(NULL);
 	}
 	
 	// create constraint
@@ -43,7 +40,7 @@ int main(int argc, char** argv){
 	md.uniq_ML = 1; // keep stuff for suboptim
 	
 	// set temperature in celsius degrees. The default is 37 Celsius
-	md.temperature = (argc < 2)?VRNA_MODEL_DEFAULT_TEMPERATURE:atof(argv[1]);
+	md.temperature = temperature;
 	
 	// create fold compound
 	vrna_fold_compound_t *fc = vrna_fold_compound(concatenated,
@@ -63,21 +60,32 @@ int main(int argc, char** argv){
 
 	// compute dimer structure
 	float mfe = vrna_mfe_dimer(fc, cstr);
+	if(mfe >= 0.0) return(NULL);
 
 	// collect suboptimal structures
 	int delta = (int)(-mfe*100.0)+1; // the range of subopt structures calculated around the optimal: delta * 0.01 kcal/mol 
-	vrna_subopt_solution_t *subopts = vrna_subopt(fc, delta, 1, NULL);
-
-	// print
-	for(vrna_subopt_solution_t *subopt = subopts; subopt->structure; ++subopt){
-		printf("%s\t%f\n", subopt->structure, subopt->energy);
-	}
+	vrna_subopt_solution_t *subopts = 
 
 	// free
 	vrna_fold_compound_free(fc);
 	free(constraint);
 	free(concatenated);
 	free(cstr);
+
+	return( subopts );
+}
+
+int main(int argc, char** argv){
+	char rna1[] = "AC\0";
+	char rna2[] = "UG\0";
+
+	vrna_subopt_solution_t *subopts = fn3(rna1, rna2, (argc < 2)?VRNA_MODEL_DEFAULT_TEMPERATURE:atof(argv[1]) );
+
+	// print
+	for(vrna_subopt_solution_t *subopt = subopts; subopt->structure; ++subopt){
+		printf("%s\t%f\n", subopt->structure, subopt->energy);
+	}
+
 	return 0;
 }
 
