3375a12ba4bbb2bbdb957c13510da7edc7ac1dab
danithered <danithered@live.com>
2025-01-07 22:37:20 +0100

    finished connect three
    sometimes it changes the original structure....
    	modified:   list.c

--- list.c
+++ list.c
@@ -4,6 +4,7 @@
 #include <ViennaRNA/fold.h>
 #include <ViennaRNA/subopt/wuchty.h>  
 #include <ViennaRNA/utils/basic.h>
+#include <ViennaRNA/utils/strings.h>
 
 vrna_subopt_solution_t* fn3(char *rna1, char *rna2, const double temperature){
 	// dynamic allocation for concatenated string
@@ -98,13 +99,216 @@ void freeSubopt(vrna_subopt_solution_t *l){
 unsigned int countLength(vrna_subopt_solution_t* x){
 	if(!x) return(0);
 	unsigned int length=0;
-	while(x[length].structure && x[length].energy>0.0) {++length;}
+	while(x[length].structure && x[length].energy<0.0) {++length;}
 	return(length);
 }
 
+void makeStickyEnds(char* begin, char* end){
+	if(!begin || !end || begin >= end) {
+		fprintf(stderr, "ERROR: non valid begin and end pointers supplied for makeStickyEnds!\n");
+		return;
+	}
+
+	char *c = begin; // iterator from first char
+	// 3' dangling end 
+	for(; c != end && *c == '.'; ++c){
+		*c = 'e';
+	}
+	
+	if(c != end){
+		char *e = end-1; // iterator from last char
+		// 5' dangling end
+		for(; *e == '.'; --e){ // no need to guard begin, as there is structure in the beginning
+			*e = 'e';
+		}
+
+		// inner part
+		for(; c != e; ++c){
+			if(*c == '.') *c = 'x';
+		}
+	}
+
+}
+
+///  binding a single sequence to the 4 possible dangling ends of a cofolded duplex
+/**
+ * Function that computes the MFE of a duplex and a simplex RNA-s binding with their 5' and 3' ends. Binding energy can be calculated with the substraction of structure MFEs from the MFE returned from this function. Please note, if you request the output of complex sequence and structure, those will have to be freed at the end, as they are allocated during the run of the function!
+ *
+ * @param[in] duplex1_seq Pointer to the sequence of the first member of the cofolded duplex RNA
+ * @param[in] duplex2_seq Pointer to the sequence of the second member of the cofolded duplex RNA
+ * @param[in] duplex_str Pointer to the dot-bracket 2D structure of the duplex RNA
+ * @param[in] single_seq Pointer to the sequence of the unfolded, single RNA
+ * @param[in] temperature Temperature of binding in Celsius degrees
+ * @param[out] compl_seq Sequence of the complex. If NULL no output will be written.
+ * @param[out] compl_str 2D stucture of the complex. If NULL no output will be written.
+ *
+ * @return MFE of the composit. If it is positive, some error happened.
+ */
+vrna_subopt_solution_t* connect3(
+		char *duplex1_seq, 
+		char *duplex2_seq, 
+		char *duplex_str,
+		char *single_seq,
+		const double temperature,
+		char **compl_seq, char **compl_str)
+{
+	const unsigned int duplex1_length = strlen(duplex1_seq);
+	const unsigned int duplex_length = strlen(duplex_str);
+	const unsigned int single_length = strlen(single_seq);
+
+	// init dynamic data
+	const unsigned int constraint_length = duplex_length + single_length + 2;
+	char *constraint = (char*) calloc(constraint_length, sizeof(char));
+	if( !constraint ){
+		fprintf(stderr, "ERROR: Could not initialise arrays in size %d\n", constraint_length);
+		return(NULL);
+	}
+	char *concat     = (char*) calloc(constraint_length, sizeof(char));
+	if( !concat ){
+		fprintf(stderr, "ERROR: Could not initialise arrays in size %d\n", constraint_length);
+		free(constraint);
+		return(NULL);
+	}	
+	char *concatstr = NULL;
+	if(compl_str){
+		concatstr  = (char*) calloc(constraint_length, sizeof(char));
+		if( !concatstr ){
+			fprintf(stderr, "ERROR: Could not initialise arrays in size %d\n", constraint_length);
+			free(constraint);
+			free(concat);
+			return(NULL);
+		}
+		memset(concatstr, '\0', constraint_length);
+	}
+
+	// create concatenated string
+	strcpy(concat, duplex1_seq);
+	concat[duplex1_length] = '&';
+	strcpy(concat + duplex1_length + 1, duplex2_seq);
+	concat[duplex_length] = '&';
+	strcpy(concat + duplex_length + 1, single_seq);
+	concat[constraint_length-1] = '\0';
+	if(compl_seq){
+		*compl_seq = (char*) calloc(constraint_length, sizeof(char));
+		if(*compl_seq) strcpy(*compl_seq, concat);
+	}
+
+	// create constraint
+	// { // duplex 5' dangling end
+		// int i = duplex_length-1;
+		// for(; i != -1 && duplex_str[i] == '.'; --i){
+			// constraint[i] = 'e';
+			// ++length5;
+		// }
+		// for(; i != -1; --i){
+			// if(duplex_str[i] == '.') constraint[i] = 'x';
+			// else constraint[i] = duplex_str[i];
+		// }
+	// }
+// 
+	// { // duplex 3' dangling end 
+		// for(unsigned int i = 0; i != duplex_length && duplex_str[i] == '.'; ++i){
+			// constraint[i] = 'e';
+		// }
+	// }
+	// constraint[duplex_length] = '&'; // separator
+// 
+	// { // single 3' dangling end 
+		// unsigned int i = 0;
+		// const unsigned int start = duplex_length+1;
+		// for(; i != single_length && single_str[i] == '.'; ++i){
+			// constraint[start+i] = 'e';
+		// }
+		// for(; i != single_length; ++i){
+			// if(single_str[i] == '.') constraint[start+i] = 'x';
+			// else constraint[start+i] = single_str[i];
+		// }
+	// }
+// 
+	// { // single 5' dangling end
+		// unsigned int i = duplex_length-1;
+		// const unsigned int start = ; 
+		// for(; i != -1 && duplex_str[i] == '.'; --i){
+			// constraint[i] = 'e';
+			// ++length5;
+		// }
+	// }
+	// constraint[constraint_length-1] = '\0'; // do not forget about terminator character 
+	
+	strcpy(constraint, duplex_str);
+	constraint[duplex_length] = '&';
+	// strcpy(constraint + duplex_length + 1, single_str);
+	memset(constraint + duplex_length + 1, 'e', single_length);
+	constraint[constraint_length-1] = '\0';
+
+	// find & separating first and second part of duplex - if it has none it is undefined behaviour!
+	// char *end = constraint + 1;
+	// while(*end != '&'){++end;}
+	
+	// make ends sticky
+	makeStickyEnds(constraint, constraint + duplex1_length); // make sticky ends for the first part of the duplex
+	makeStickyEnds(constraint + duplex1_length + 1, constraint + duplex_length); // make sticky ends for the second part of the duplex
+							      //
+	// makeStickyEnds(constraint, end-1); // make sticky ends for the first part of the duplex
+	// makeStickyEnds(end+1, constraint + duplex_length -1); // make sticky ends for the second part of the duplex
+	// makeStickyEnds(constraint + duplex_length + 1, constraint + constraint_length - 1); // make single ends sticky
+
+	// printf("constraint: %s\n", constraint);
+
+
+	/* create a new model details structure to store the Model Settings */
+	vrna_md_t md;
+	vrna_md_set_default(&md); 
+	
+	// set temperature in celsius degrees
+	md.temperature = temperature;
+	
+	// create fold compound
+	vrna_fold_compound_t *fc = vrna_fold_compound(concat,
+                                                &md,//&(opt->md),
+                                                VRNA_OPTION_DEFAULT | VRNA_OPTION_HYBRID);
+
+	// add hard constraint
+	vrna_constraints_add(fc, constraint,
+			//VRNA_CONSTRAINT_DB | 
+			VRNA_CONSTRAINT_DB_X | 
+			VRNA_CONSTRAINT_DB_INTERMOL |
+			//VRNA_CONSTRAINT_DB_INTRAMOL |
+			VRNA_CONSTRAINT_DB_DEFAULT |
+			//VRNA_CONSTRAINT_DB_ENFORCE_BP |
+			VRNA_CONSTRAINT_DB_PIPE
+			);
+	
+	// compute dimer structure
+	float mfe = vrna_mfe_dimer(fc, concatstr);
+	if(mfe >= 0.0) {
+		vrna_fold_compound_free(fc);
+		free(constraint);
+		free(concat);
+		free(concatstr);
+		return(NULL);
+	}
+      	
+	// collect suboptimal structures
+	int delta = (int)(-mfe*100.0)+1; // the range of subopt structures calculated around the optimal: delta * 0.01 kcal/mol 
+	vrna_subopt_solution_t *subopts = vrna_subopt(fc, delta, 1, NULL); 
+
+	// free stuff
+	vrna_fold_compound_free(fc);
+	free(constraint);
+	free(concat);
+	free(concatstr);
+
+	return( subopts );
+}
+
+
 int main(int argc, char** argv){
-	char rna1[] = "AC\0";
-	char rna2[] = "UG\0";
+	char rna1[] = "ACCCCC\0";
+	char rna2[] = "UGGGCC\0";
+	char rna3[] = "AGGG\0";
+
+	printf("Connecting RNA sequences %s and %s\n", rna1, rna2);
 
 	vrna_subopt_solution_t *subopts = fn3(rna1, rna2, VRNA_MODEL_DEFAULT_TEMPERATURE );
 
@@ -117,6 +321,27 @@ int main(int argc, char** argv){
 	// iterating tru list
 	for(vrna_subopt_solution_t *subopt = subopts; subopt->structure; ++subopt){
 		printf("%s\t%f\n", subopt->structure, subopt->energy); // printing it
+	}
+
+	const unsigned int no_subopt = countLength(subopts);
+	if(no_subopt){
+		printf("Connecting first RNA duplex of %d suboptimal structures and rna %s\n", no_subopt, rna3);
+		vrna_subopt_solution_t *subopts2 = connect3(
+				rna1, rna2, // the seq of the 1st and 2nd member of the cofolded complex
+				subopts[0].structure, // structure of the cofolded complex
+				rna3, // the single rna to bind to the complex
+				VRNA_MODEL_DEFAULT_TEMPERATURE, // temperature
+				NULL, NULL); // outputs
+		// print
+		if(!subopts2){ // if there was no beneficial structure print it
+			printf("Could not find optimal solutions.\n");
+			return(1);
+		}
+
+		// iterating tru list
+		for(vrna_subopt_solution_t *subopt = subopts2; subopt->structure; ++subopt){
+			printf("%s\t%f\n", subopt->structure, subopt->energy); // printing it
+		}
 	}
 
 	// free and return
